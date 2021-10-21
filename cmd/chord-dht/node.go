1dd3e87b44b4d258d628885b6a19ed372107dd45
quarterblue <quarterslate@gmail.com>
2021-10-20 21:31:23 -0700

    Structure added

--- cmd/chord-dht/node.go
+++ cmd/chord-dht/node.go
@@ -1,24 +1,36 @@
 package main
 
+import "context"
+
 // m-bit hash of the node IP Addr or the key (Default uses SHA-1)
 type Identifier []byte
 
 type FingerTable map[string]Finger
 
 type Node struct {
-	finger      FingerTable
-	successor   Entry
+	// The route table
+	finger FingerTable
+
+	// The next node on the identifier cirle; finger[1].node
+	successor Entry
+
+	// The previous node on the identifier circle
 	predecessor Entry
-	identifier  Identifier
+
+	// The m-bit identifier
+	identifier Identifier
 }
 
 type Finger struct {
 	// (n + 2^(i-1)) mod 2^m (1 <= i <= m)
 	start int
+
 	// (n + 2^i - 1) mod 2^m
 	end int
+
 	// interval
 	interval [2]int
+
 	// succesor node
 	successor int
 }
@@ -34,8 +46,24 @@ type Entry struct {
 	identifier Identifier
 }
 
-func (n *Node) findSuccessor(id string) (*Entry, error) {
-	return nil, nil
+func (n *Node) findSuccessor(ctx context.Context, id Identifier) (*Entry, error) {
+	if withinFingerRange(n.identifier, id) {
+		// return n.finger[id], nil
+		return nil, nil
+	} else {
+		// nPrime := closestPrecedingNode(id)
+		// return nPrime.findSuccessor(ctx, id)
+
+		return nil, nil
+	}
+}
+
+func closestPrecedingNode(id Identifier) *Entry {
+	return nil
+}
+
+func withinFingerRange(n, successor Identifier) bool {
+	return true
 }
 
 func NewEntry(ipAddr, port string, identifier []byte) *Entry {
@@ -51,7 +79,7 @@ func NewNode() {
 
 }
 
-func InitFingerTable(nPrime Entry) error {
-
-	return nil
+func InitFingerTable() (FingerTable, error) {
+	newFT := make(map[string]Finger)
+	return newFT, nil
 }
